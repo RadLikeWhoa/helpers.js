b4a29697774cb7740038fcb9f0f62af284cfe16a
Sacha Schmid <schmid.sacha@gmail.com>
2012-12-12 09:38:30 +0100

    Update wordcount documentation to use ScriptDoc.

--- wordcount.js
+++ wordcount.js
@@ -1,13 +1,13 @@
 /**
- * Name                   wordcount()
- * Description            Count the words inside a given element. The output
- *                        can be formatted using the {count} placeholder.
+ * Count the words inside a given element. The output can be formatted using the
+ *   {count} placeholder.
  *
- * wordcount('Hello, it\'s me', 'This is {count} words long.')
+ * @example   wordcount('Hello, it\'s me', 'This is {count} words long.')
  *
- * @param     {string}    text
- * @param     {string}    format
- * @return    {string} / {number}
+ * @param     {string}    text      The text to be counted.
+ * @param     {string}    format    The format used for the output. Use `{count}` as a placeholder. (default: {count})
+ * 
+ * @return    {string, number}      Returns the number of words or the format.
  */
 
 var wordcount = function (text, format) {
