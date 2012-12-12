f7eee476d4cf654cfe4a86a4aced7c967c81e254
Sacha Schmid <schmid.sacha@gmail.com>
2012-12-12 09:30:50 +0100

    Update baseURL documentation to use ScriptDoc.

--- baseURL.js
+++ baseURL.js
@@ -1,13 +1,13 @@
 /**
- * Name                   baseURL()
- * Description            Find only the root name of any given URL. Optionally,
- *                        the Top Level Domain can be hidden as well.
+ * Find only the root name of any given URL. Optionally,
+ *   the Top Level Domain can be hidden as well.
  *
- * baseURL('http://www.maxvoltar.com/archive/more-beats')
+ * @example   baseURL('http://www.maxvoltar.com/archive/more-beats')
  *
- * @param     {string}    url
- * @param     {bool}      hideTLD
- * @return    {string}
+ * @param     {string}    url           The url to be parsed.
+ * @param     {bool}      [hideTLD]     Show or hide the Top-Level domain (e.g. .com). (default: false)
+ * 
+ * @return    {string}    Returns the root of the URL.
  */
 
 var baseURL = function (url, hideTLD) {
